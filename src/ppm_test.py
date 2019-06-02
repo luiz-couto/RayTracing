b2f25313a69c1bdd3dd91a8e097cdffa855c16d3
luiz-couto <luiz.felipe.556@outlook.com>
2019-06-02 12:28:57 -0300

    Added surface normals

--- src/ppm_test.py
+++ src/ppm_test.py
@@ -2,6 +2,7 @@
 #TEST PPM FILE
 
 import glm
+import math
 
 
 class Ray:
@@ -10,7 +11,9 @@ class Ray:
         self.b = glm.vec3(b)
         self.origin = self.a
         self.direction = self.b
-        
+       
+    def point_at_parameter(self,t):
+        return self.a + t*self.b  
 
 def hit_sphere(center,radius,r):
     oc = glm.vec3(r.origin - center)
@@ -18,13 +21,18 @@ def hit_sphere(center,radius,r):
     b = 2.0*glm.dot(oc,r.direction)
     c = glm.dot(oc,oc) - radius*radius
     discriminant = b*b - 4*a*c
-    return(discriminant > 0)
+    if(discriminant < 0):
+        return -1.0
+    if(discriminant >= 0):
+        return ((-b - math.sqrt(discriminant))/(2.0*a))
 
 def color(r):
     
-    if(hit_sphere(glm.vec3(0,0,-1),0.5,r)):
-        return glm.vec3(1,0,0)
-
+    t = hit_sphere(glm.vec3(0,0,-1),0.5,r)
+    
+    if(t > 0.0):
+        n = glm.vec3(r.point_at_parameter(t) - glm.vec3(0,0,-1)/glm.length(r.point_at_parameter(t) - glm.vec3(0,0,-1)))
+        return 0.5*glm.vec3(n[0]+1.0,n[1]+1.0,n[2]+1.0)
     unit_direction = glm.vec3(r.direction/glm.length(r.direction))
     t = 0.5*(unit_direction[1] + 1.0)
     return (1.0 - t)*glm.vec3(1.0,1.0,1.0) + t*glm.vec3(0.5,0.7,1.0)
@@ -49,6 +57,7 @@ def main():
             v = j/ny
             r = Ray(origin,lower_left_corner + u*horizontal + v*vertical)
             col = glm.vec3(color(r))
+
             ir = int(255.99*col[0])
             ig = int(255.99*col[1])
             ib = int(255.99*col[2])
